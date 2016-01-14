3d0d8f2e73e8885792172021150b5e0878a03daa
dasparadoxon <peter@dasparadoxon.de>
2016-01-14 19:17:51 +0100

    Shorter timeout, ignoring unused IPs
    * Shorter Timeout
    * Using netaddr to ignore non-accesible IPs

--- fakingIt.py
+++ fakingIt.py
@@ -3,10 +3,9 @@
 # FAKINGIT - A TOOL TO JUST RANDOMLY CALL WEBSITES ON RANDOM SERVERS
 #
 # IF ENOUGH PPL WOULD USE SOMETHING LIKE THIS, THIS COULD SERIOUSLY MESS UP
-# ANY PATTERN SURVILLANCE WITHOUT REAL CLUES WHERE THE REAL USE OF FINDING
-# SERIOUS CRIMINALITY WOULD STAY THE SAME
+# ANY PATTERN SURVILLANCE WITHOUT REAL CLUES
 #
-# FREE SOFTWARE ! USE IT AT YOUR OWN RISK ETC.ETC.ETC.
+# FREE SOFTWARE USE IT AT YOUR OWN RISK ETC.ETC.ETC.
 #
 ###
 
@@ -14,6 +13,9 @@ import logging
 import random
 import requests
 import time
+
+from netaddr import *
+import pprint
 
 logger = None
 
@@ -25,22 +27,152 @@ class fakingIt():
 
     def makeRandomRequest(self,round):
 
-        ip1 = random.randint(1, 255)
-        ip2 = random.randint(1, 255)
-        ip3 = random.randint(1, 255)
-        ip4 = random.randint(1, 255)
+        valid_address = False
+
+        while valid_address == False:
+
+            valid_address = True
+
+            ip1 = random.randint(1, 255)
+            ip2 = random.randint(1, 255)
+            ip3 = random.randint(1, 255)
+            ip4 = random.randint(1, 255)
+
+            # SOURCE : 1) Wikipedia 2) http://www.heise.de/netze/Reservierte-IPv4-Adressen-475028.html
+
+            network_list = []
+
+            network_list.append(IPNetwork('10.0.0.0/24'))
+            if(ip1 == 10):
+                valid_address = False
+
+            network_list.append(IPNetwork('192.0.0.0/24'))
+            if(ip1 == 192):
+                valid_address = False
+
+            #network_list.append(IPNetwork('172.0.0.0/24'))
+            if(ip1 == 172):
+                if(ip2>15 and ip2<33):
+                    valid_address = False
+
+            network_list.append(IPNetwork('192.168.0.0/16'))
+            if(ip1 == 192):
+                if(ip2==168):
+                    valid_address = False
+
+            network_list.append(IPNetwork('127.0.0.0/8'))
+            # 127.0.0.0/8
+            if(ip1 == 127):
+                if(ip2==0):
+                    if(ip3==0):
+                        valid_address = False
+
+            network_list.append(IPNetwork('192.0.0.0/24'))
+            # 192.0.0.0/24 (RFC 5736)
+            if(ip1 == 192):
+                if(ip2==0):
+                    if(ip3==0):
+                        valid_address = False
+
+            network_list.append(IPNetwork('169.254.0.0/16'))
+            # RFC 5735 als Link Local (RFC 3927 ?)
+            if(ip1 == 169):
+                if(ip2==254):
+                    valid_address = False
+
+
+            #172.16.0.0/12 (RFC 1918)
+            network_list.append(IPNetwork('172.16.0.0/12'))
+            if(ip1 == 172):
+                if(ip2==16):
+                    if(ip3>240):
+                        valid_address = False
+
+
+            # 192.0.2.0/24,  ,  (RFC 5737)
+            network_list.append(IPNetwork('192.0.2.0/24'))
+            if(ip1 == 192):
+                if(ip2==0):
+                    if(ip3==2):
+                        valid_address = False
+
+            #198.51.100.0/24 ,  (RFC 5737)
+            network_list.append(IPNetwork('198.51.100.0/24'))
+            if(ip1 == 198):
+                if(ip2==51):
+                    if(ip3==100):
+                        valid_address = False
+
+            #203.0.113.0/24 ,  (RFC 5737)
+            network_list.append(IPNetwork('203.0.113.0/24'))
+            if(ip1 == 203):
+                if(ip2==0):
+                    if(ip3==113):
+                        valid_address = False
+
+            # 192.88.99.0/24
+            network_list.append(IPNetwork('192.88.99.0/24'))
+            if(ip1 == 192):
+                if(ip2==88):
+                    if(ip3==99):
+                        valid_address = False
+
+            # 198.18.0.0/15 - RFC 2544
+            network_list.append(IPNetwork('198.18.0.0/15'))
+            if(ip1 == 198):
+                if(ip2==18):
+                    if(ip3>253):
+                        valid_address = False
+
+
+            ip  = str(ip1)+"."+str(ip2)+"."+str(ip3)+"."+str(ip4)
+
+            ipobj = IPAddress(ip)
+
+            if ipobj.is_multicast():
+                valid_address = False
+
+            if ipobj.is_reserved():
+                valid_address = False
+
+            if ipobj.is_private():
+                valid_address = False
+
+            if ipobj.is_unicast() and not ipobj.is_private():
+                valid_address = True
+
+
+            if valid_address == False:
+                print("")
+                msg = "Invalid IP : "+ip
+                logger.error(msg)
+                print(msg)
+
 
         url = "http://"+str(ip1)+"."+str(ip2)+"."+str(ip3)+"."+str(ip4)
 
         logger.info(str(round)+" - "+url);
 
+        msg = "Trying to access web-port at IP : "+url
+
+        #print(msg)
+
+        #url = "http://www.google.com"
+
         try:
-            r = requests.get(url)
+            r = requests.get(url, timeout=0.75)
 
         except Exception as ex:
             template = "An exception of type {0} occured. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
-            print message
+            r = None
+            #print message
+
+        if r != None:
+
+            msg = "A connection worked to IP / URL : "+url
+            print(msg)
+            logger.info(msg);
 
 
 # SETTING UP LOGGING
@@ -65,6 +197,8 @@ running = True
 while(running):
 
     fakeIt.makeRandomRequest(round)
+
+    print(str(round)+','),
 
     time.sleep(1)
 
